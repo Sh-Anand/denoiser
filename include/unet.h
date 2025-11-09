557fc5952d9d2d51068a814f25a3cff7afb10d90
Shashank Anand <shashank.anand@berkeley.edu>
2025-11-08 18:03:41 -0800

    unet skeleton

---
+++ include/unet.h
@@ -0,0 +1,4 @@
+#include "exr.h"
+#include "tza.h"
+
+void oidn_unet(EXR& input_img, TzaFile& weights, EXR& output_img);
---
+++ src/unet/unet.cpp
@@ -0,0 +1,16 @@
+#include <array>
+#include <cstddef>
+#include <stdexcept>
+#include <stdio.h>
+#include <utility>
+
+#include "conv.h"
+#include "exr.h"
+#include "tza.h"
+#include "unet.h"
+
+void oidn_unet(EXR& input_img,
+               TzaFile& weights,
+               EXR& output_img) {
+
+}
