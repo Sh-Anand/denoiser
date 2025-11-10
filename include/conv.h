45e01898e4f9701a3b425e45d7e27dd3f2fdf95b
Shashank Anand <shashank.anand@berkeley.edu>
2025-11-09 16:28:06 -0800

    nuke useless std:: in size_t

--- include/conv.h
+++ include/conv.h
@@ -2,26 +2,26 @@
 
 void conv_relu_nhwc_oihw(const float* input,
                         float* output,
-                        std::size_t in_h,
-                        std::size_t in_w,
-                        std::size_t filter_h,
-                        std::size_t filter_w,
-                        std::size_t in_c,
-                        std::size_t out_c,
+                        size_t in_h,
+                        size_t in_w,
+                        size_t filter_h,
+                        size_t filter_w,
+                        size_t in_c,
+                        size_t out_c,
                         const _Float16* weights,
                         const _Float16* bias);
 
 void max_pool_nhwc(const float* input,
                    float* output,
-                   std::size_t in_h,
-                   std::size_t in_w,
-                   std::size_t in_c);
+                   size_t in_h,
+                   size_t in_w,
+                   size_t in_c);
 
 void avg_pool_nhwc(const float* input,
                    float* output,
-                   std::size_t in_h,
-                   std::size_t in_w,
-                   std::size_t in_c);
+                   size_t in_h,
+                   size_t in_w,
+                   size_t in_c);
 
 void nn_upsample_nhwc(const float* input,
                       float* output,
